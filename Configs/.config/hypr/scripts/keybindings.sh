617a31e1f072b6ece0b61732a9a3c6656f4ee7d4
devCKVargas <vargaschrisking@gmail.com>
2023-12-01 15:34:57 +0800

    feat(keybindings): add keyboard shortcuts hint & fix waybar position
    - Add keybinds waybar module
    - This should fix the initial waybar position

---
+++ Configs/.config/hypr/scripts/keybindings.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env sh
+#     _          ___ _  __
+#  __| |_____ __/ __| |/ /
+# / _` / -_) V / (__| ' < 
+# \__,_\___|\_/ \___|_|\_\                         
+
+yad --width=800 --height=650 \
+--center \
+--title="Keybindings" \
+--list \
+--column=Key: \
+--column=Description: \
+"====================" "====================" \
+" = SUPER" "Window Key" \
+" + /" "Open keyboard shortcuts hint (This window)" \
+"🄰🄿🄿🅂" "====================" \
+"terminal" "kitty" \
+"editor" "code (vscode)" \
+"file manager" "dolphin" \
+"browser" "firefox" \
+"🅆🄸🄽🄳🄾🅆/🅂🄴🅂🅂🄸🄾🄽" "====================" \
+" + Q" "Kill focus window" \
+"ALT + F4" "Kill focus window" \
+" + delete" "Kill hyprland session" \
+" + W" "Toggle floating of focus window" \
+" + G" "Toggle grouping of focus window" \
+"🄰🄿🄿 🅂🄷🄾🅁🅃🄲🅄🅃🅂" "====================" \
+"ALT + return" "Toggle fullscreen of focus window" \
+" + L" "Lock the screen" \
+" + backspace" "Logout menu" \
+"Ctrl + ESC" "Toggle waybar" \
+" + T" "Terminal" \
+" + E" "File manager" \
+" + C" "vscode" \
+" + F" "browser(firefox)" \
+"Ctrl + Shift + ESC" "Open htop/btop or top (whichever is installed)" \
+"🅁🄾🄵🄸" "====================" \
+" + A" "Rofi menu" \
+" + tab" "Switch between open app window" \
+" + R" "Browse system files" \
+"🄰🅄🄳🄸🄾" "====================" \
+"󰖁" "Mute audio" \
+"󰍭" "Mute mic" \
+"󰝞  (Hold)" "Decrease volume" \
+"󰝝  (Hold)" "Increase volume" \
+"󰐎" "play-pause" \
+"󰐎" "play-pause" \
+"󰒭" "next" \
+"󰒮" "previous" \
+"🄱🅁🄸🄶🄷🅃🄽🄴🅂🅂" "====================" \
+"󰃠 󰁝  (Hold)" "Increase brightness" \
+"󰃠 󰁅  (Hold)" "Decrease down" \
+"🅂🄲🅁🄴🄴🄽🅂🄷🄾🅃" "====================" \
+" + P" "Snipping area" \
+" + ALT + P" "Screenshot focused monitor" \
+"PrintScreen" "Screenshot all monitors" \
+"🄻🄾🄾🄺 🄰🄽🄳 🄵🄴🄴🄻" "====================" \
+" + ALT + G" "Disable hypr effects / Gamemode" \
+" + ALT + 󰜱󰜴" "Change wallpaper" \
+" + ALT + 󰜷󰜮" "Change waybar mode / position" \
+" + Shift + D" "Toggle wallbash" \
+" + Shift + T" "Theme menu" \
+" + ALT +" "Rofi style menu" \
+" + ALT +" "Rofi wallpaper menu" \
+" + ALT +" "Clipboard" \
+" + ALT +" "Change keyboard layout" \
+"🅆🄾🅁🄺🅂🄿🄰🄲🄴" "====================" \
+" + ALT + 󰜱󰜴󰜷󰜮" "Move focus" \
+"ALT + Tab" "Move focus" \
+" + (0-9)" "Switch workspaces" \
+" + Ctrl 󰜱󰜴" "Switch workspace relative to the current one" \
+" + Ctrl 󰜮" "Switch to the first empty workspace" \
+" + Shift 󰜱󰜴󰜷󰜮" "Resize focus window" \
+" + Shift + (0-9)" "Move active window to a workspace" \
+" + Ctrl + ALT + 󰜱󰜴" "Move active window to a relative workspace" \
+" + Shift + Ctrl 󰜱󰜴󰜷󰜮" "Move active window around current workspaces" \
+" + Scrollwheel  󱕒" "Scroll through existing workspaces" \
+" + Left 󰍽  (Hold)" "Move window" \
+" + Right 󰍽  (Hold)" "Resize window" \
+" + Alt + S" "Move window to special workspace (Scratchpad)" \
+" + S" "Toggle special workspace (Scratchpad)" \
+" + J" "Toggle split (dwindle)" \
+"Super + Alt + (0-9)" "Move window silently to workspace" \
+"🅂🅈🅂🅃🄴🄼 🄱🅄🅃🅃🄾🄽" "====================" \
+"⏻" "Suspend system" \
